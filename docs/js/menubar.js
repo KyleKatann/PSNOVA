eb1dae4face507bfd33b56e8716e0193c3d6d5ee
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2019-12-24 06:43:05 +0900

    ready for global

---
+++ docs/js/menubar.js
@@ -0,0 +1,32 @@
+function menu(){
+var html =`
+
+
+<!--PC用（801px以上端末）メニュー-->
+<nav id="menubar">
+<ul>
+<li><a href="index.html">ホーム<span>HOME</span></a></li>
+<li><a href="company.html">会社概要<span>COMPANY</span></a></li>
+<li><a href="service.html">サービス紹介<span>SERVICE</span></a></li>
+<li><a href="recruit.html">採用情報<span>RECRUIT</span></a></li>
+<li><a href="faq.html">よく頂く質問<span>FAQ</span></a></li>
+<li><a href="contact.html">お問い合わせ<span>CONTACT</span></a></li>
+<li><a href="contact.html">お問い合わせ<span>CONTACT</span></a></li>
+</ul>
+</nav>
+
+<!--小さな端末用（800px以下端末）メニュー-->
+<nav id="menubar-s">
+<ul>
+<li><a href="index.html">ホーム</a></li>
+<li><a href="company.html">会社概要</a></li>
+<li><a href="service.html">サービス紹介</a></li>
+<li><a href="recruit.html">採用情報</a></li>
+<li><a href="faq.html">よく頂く質問</a></li>
+<li><a href="contact.html">お問い合わせ</a></li>
+</ul>
+</nav>
+
+`;
+document.write(html);
+}
---
+++ docs/js/sidebar.js
@@ -0,0 +1,29 @@
+function side(){
+    var html =`
+    
+    
+    <div id="sub">
+    <nav>
+    <h2>Contents</h2>
+    <ul class="submenu">
+    <li><a href="company.html">会社概要</a></li>
+    <li><a href="service.html">サービス紹介</a></li>
+    <li><a href="recruit.html">採用情報</a></li>
+    <li><a href="faq.html">よく頂く質問</a></li>
+    <li><a href="link.html">リンク</a></li>
+    <li><a href="contact.html">お問い合わせ</a></li>
+    </ul>
+    </nav>
+
+    <aside>
+<!--
+    <a href="recruit.html"><img src="images/banner1.jpg" alt="採用情報" class="pc"></a>
+    <a href="recruit.html"><img src="images/banner1_sh.jpg" alt="採用情報" class="sh"></a>  
+-->
+    </aside>
+</div>
+
+    
+    `;
+    document.write(html);
+    }
