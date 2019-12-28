187caa6e45d3ffc4afcf58214bddafae1e4787e6
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2019-12-29 00:46:11 +0900

    fix

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -7,27 +7,30 @@ var html =`
 <nav>
 <h2>Contents</h2>
 <ul class="submenu">
-<li><p>各種データ</p></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/weapon.html">武器データ</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/armor.html">防具データ</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/attachment.html">アタッチパーツ</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/specialability.html">特殊能力</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/material.html">素材</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/item.html">消費アイテム</a></li>
 
-<li><p>クエスト</p></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/difficulty.html">難易度</a></li>
+    <li><p>各種データ</p></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/weapon.html">武器データ</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/armor.html">防具データ</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/attachment.html">アタッチパーツ</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/specialability.html">特殊能力</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/material.html">素材</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/item.html">消費アイテム</a></li>
 
-<li><p>キャラクター</p></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/species.html">種族</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/appearance.html">ヘアスタイル・コスチューム・アクセサリー</a></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/class.html">クラス</a></li>
-<!--  <li><a href="https://kylekatann.github.io/PSNOVA/pages/skill.html">スキル一覧</a></li>  -->
+    <li><p>クエスト</p></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/difficulty.html">難易度</a></li>
+
+    <li><p>キャラクター</p></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/species.html">種族</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/appearance.html">ヘアスタイル・コスチューム・アクセサリー</a></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/class.html">クラス</a></li>
+    <!--  <li><a href="https://kylekatann.github.io/PSNOVA/pages/skill.html">スキル一覧</a></li>  -->
+
+    <li><p>トロフィー</p></li>
+    <li><a href="https://kylekatann.github.io/PSNOVA/pages/trophy.html">トロフィー</a></li>
+
 </ul>
 </nav>
 
-<li><p>トロフィー</p></li>
-<li><a href="https://kylekatann.github.io/PSNOVA/pages/trophy.html">トロフィー</a></li>
 
 <aside>
 <!--
