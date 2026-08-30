900442ead426286c133f87e08935cec4b749ddd1
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2026-08-30 15:57:00 +0900

    ui: remove obsolete top navigation links

--- docs/js/menubar.js
+++ docs/js/menubar.js
@@ -28,35 +28,5 @@
     }
 })();
 
-function menu(){
-var html =`
-<!--PC用（801px以上端末）メニュー-->
-<nav id="menubar">
-<ul>
-<li><a href="/PSNOVA/index.html">ホーム<span>HOME</span></a></li>
-<li><a href="/PSNOVA/copyright.html">著作権表示<span>copyright</span></a></li>
-<li><a href="/PSNOVA/issue.html">修正・加筆要望<span>issue</span></a></li>
-</ul>
-</nav>
-
-<!--小さな端末用（800px以下端末）メニュー-->
-<nav id="menubar-s">
-<ul>
-<li><a href="/PSNOVA/index.html">ホーム<span>HOME</span></a></li>
-<li><a href="/PSNOVA/copyright.html">著作権表示<span>copyright</span></a></li>
-<li><a href="/PSNOVA/issue.html">修正・加筆要望<span>issue</span></a></li>
-</ul>
-</nav>
-`;
-
-var callSite = document.currentScript;
-if (callSite) {
-    callSite.insertAdjacentHTML("beforebegin", html);
-    return;
-}
-
-var header = document.querySelector("#container > header");
-if (header) {
-    header.insertAdjacentHTML("afterend", html);
-}
-}
+/* Legacy top navigation removed. Kept as a compatibility no-op because static pages still call menu(). */
+function menu() {}
