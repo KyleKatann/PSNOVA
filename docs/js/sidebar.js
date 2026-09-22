caaa89e9f8449b2cba2f6de6cc88940ddcf596f4
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2026-09-22 18:52:51 +0900

    サイドバーにエネミーレーダーを追加

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -91,6 +91,7 @@ var html =`
         <a href="/PSNOVA/pages/base.html">拠点施設</a>
         <ul class="weapon-submenu" aria-label="拠点施設">
             <li><a href="/PSNOVA/pages/search-corps.html">探索隊司令部</a></li>
+            <li><a href="/PSNOVA/pages/enemy-radar.html">エネミーレーダー</a></li>
         </ul>
     </li>
     <li><a href="/PSNOVA/pages/food.html">食事</a></li>
@@ -148,7 +149,7 @@ function markCurrentSidebarLink(){
     var technicChild = /^\/PSNOVA\/pages\/technic\/[^/]+\.html$/.test(currentPath);
     var questChild = /^\/PSNOVA\/pages\/quest\/[^/]+\.html$/.test(currentPath);
     var promiseOrderChild = /^\/PSNOVA\/pages\/promise-order\/[^/]+\.html$/.test(currentPath);
-    var baseChild = currentPath === "/PSNOVA/pages/search-corps.html";
+    var baseChild = currentPath === "/PSNOVA/pages/search-corps.html" || currentPath === "/PSNOVA/pages/enemy-radar.html";
     var appearanceChild = /^\/PSNOVA\/pages\/appearance\/[^/]+\.html$/.test(currentPath);
     var links = document.querySelectorAll("#sub .submenu a[href]");
 
